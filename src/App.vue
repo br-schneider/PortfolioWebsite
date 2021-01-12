a754f833ba9e02c84f5024052d3756a2c83b2fd0
Brett Schneider <brett@Bretts-MacBook-Pro.local>
2021-01-12 13:17:20 -0600

    Front formatting fixed

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
-  <header class="relative flex bg-gray-100 sm:flex sm:justify-between sm:px-4 sm:items-center sm:py-4">
+  <header class="container mx-auto bg-gray-100 sm:flex sm:justify-between sm:items-center sm:py-4">
     <div class="flex items-center justify-between px-4 py-3 sm:p-0">
       
       <div>
         <h1 class="text-2xl font-bold text-gray-800">Brett Schneider</h1>
       </div>
       
-      <div class="">
+      <div class="mt-2">
         <button @click="isOpen=!isOpen" class="sm:hidden text-grey-500 hover:text-buffGold focus:text-blue focus:outline-none" onClick={openMenu}>
           <svg class="w-6 h-6 fill-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path v-if="isOpen" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
@@ -17,11 +17,11 @@
       
     </div>
       
-    <div :class="isOpen ? 'block' : 'hidden' " class="px-2 text-left sm:flex" >
+    <div :class="isOpen ? 'block' : 'hidden' " class="items-center px-2 text-left sm:flex" >
       <router-link to="/" class="block px-2 py-1 font-medium text-gray-800 rounded hover:text-gray-300 sm:ml-2">Home</router-link>
       <router-link to="/about" class="block px-2 py-1 mt-1 font-medium text-gray-800 rounded hover:text-gray-300 sm:mt-0 sm:ml-2">Projects</router-link>
       <a href="#" class="block px-2 py-1 mt-1 font-medium text-gray-800 rounded hover:text-gray-300 sm:mt-0 sm:ml-2"> Photography </a>
-      <a href="#" class="block px-2 py-1 mt-1 mb-2 font-medium text-gray-800 rounded hover:text-gray-300 sm:mt-0 sm:ml-2"> Contact Me </a>
+      <a href="#" class="block px-2 py-1 mt-1 mb-2 font-medium text-gray-800 rounded sm:mb-0 hover:text-gray-300 sm:mt-0 sm:ml-2"> Contact Me </a>
     </div>  
 
   </header>
