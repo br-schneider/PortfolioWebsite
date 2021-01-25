79f97c8e1e3a5c6cd245014ba0d00a1093b48216
Brett Schneider <brett@Bretts-MacBook-Pro.local>
2021-01-25 16:19:00 -0700

    Update Contact.vue

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -88,7 +88,7 @@
             <textarea id="message" name="message" rows="4" v-model="messageMsg" class="block w-full px-4 py-3 placeholder-gray-500 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500" placeholder="Message"></textarea>
           </div>
           <div>
-            <button @click="clicked=!clicked "  type="submit" class="inline-flex justify-center px-6 py-3 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+            <button @click="clicked=!clicked " :disabled="!nameMsg || !emailMsg || !messageMsg"  type="submit" class="inline-flex justify-center px-6 py-3 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
               Submit
             </button>
           </div>
